15ae5b13c5da622045f10c68143a67283f606c9b
ajaykpathak <ajay@realmonkey.ai>
2025-10-07 20:02:04 +0530

    Updated URLS.PY added a home page

--- backend/urls.py
+++ backend/urls.py
@@ -17,10 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Budget calculator is running fine on Render !!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('tracker.urls')),
-    path('api-token-auth/', obtain_auth_token),
+    path('api-token-auth/', obtain_auth_token), 
     path('', home),
 ]
